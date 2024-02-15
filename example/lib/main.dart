1c3b8612e434952b89968c1653edc2f49dad3793
Dhruv Singh <dhurv.singh@highspot.com>
2024-02-15 16:23:54 +0530

    second-commit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -140,7 +140,6 @@ class _MainPageState extends State<MainPage> {
               children: [
                 PdfViewer.asset(
                   'assets/hello.pdf',
-
                   // PdfViewer.file(
                   //   r"D:\pdfrx\example\assets\hello.pdf",
                   // PdfViewer.uri(
